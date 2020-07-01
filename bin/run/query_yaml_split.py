35aaf8d307a15c5a22b64d2c404f4724c5268f16
Wayne Schmidt <wschmidt@sumologic.com>
2020-07-01 09:50:24 +0900

    added and reorganized

---
+++ bin/run/query_yaml_split.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Sample YAML extractor of query strings into .sqy
+"""
+import os
+import re
+import sys
+from benedict import benedict
+yaml_file = sys.argv[1]
+yaml_dir = os.path.dirname(os.path.abspath(yaml_file))
+yaml_dict = benedict.from_yaml(yaml_file)
+for keypath in benedict.keypaths(yaml_dict):
+    if re.match(r".*?\.query$", keypath):
+        query_file = os.path.join(yaml_dir, keypath + '.sqy')
+        print(query_file)
+        with open(query_file, 'w') as file_object:
+            file_object.write(yaml_dict[keypath])
