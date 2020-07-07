01db4a9debc20f5a8b46fbdd1335912948269e70
Wayne Schmidt <wschmidt@sumologic.com>
2020-07-07 20:56:28 +0900

    renamed template

---
+++ lib/templates/sumocli_standalone_template.py
@@ -0,0 +1,207 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Exaplanation: sumocli_standard_template
+
+Usage:
+   $ python  sumo_query [ options ]
+
+Style:
+   Google Python Style Guide:
+   http://google.github.io/styleguide/pyguide.html
+
+    @name           sumocli_standard_template
+    @version        0.90
+    @author-name    Wayne Schmidt
+    @author-email   wschmidt@sumologic.com
+    @license-name   GNU GPL
+    @license-url    http://www.gnu.org/licenses/gpl.html
+"""
+
+__version__ = 0.90
+__author__ = "Wayne Schmidt (wschmidt@sumologic.com)"
+
+import json
+import pprint
+import os
+import sys
+import argparse
+import http
+import re
+import time
+import requests
+sys.dont_write_bytecode = 1
+
+MY_CFG = 'undefined'
+PARSER = argparse.ArgumentParser(description="""
+
+sumo_query is a cmdlet managing queries.
+
+""")
+
+PARSER.add_argument("-a", metavar='<secret>', dest='MY_SECRET', \
+                    help="set api (format: <key>:<secret>) ")
+PARSER.add_argument("-k", metavar='<client>', dest='MY_CLIENT', \
+                    help="set key (format: <site>_<orgid>) ")
+PARSER.add_argument("-e", metavar='<endpoint>', default='us2', dest='MY_ENDPOINT', \
+                    help="set endpoint (format: <endpoint>) ")
+PARSER.add_argument("-o", metavar='<fmt>', default="csv", dest='OUT_FORMAT', \
+                    help="set query_output (values: txt, csv)")
+PARSER.add_argument("-v", type=int, default=0, metavar='<verbose>', \
+                    dest='VERBOSE', help="increase verbosity")
+
+ARGS = PARSER.parse_args()
+
+SEC_M = 1000
+MIN_S = 60
+HOUR_M = 60
+DAY_H = 24
+WEEK_D = 7
+
+CSV_SEP = ','
+TAB_SEP = '\t'
+EOL_SEP = '\n'
+
+MY_SEP = CSV_SEP
+if ARGS.OUT_FORMAT == 'txt':
+    MS_SEP = TAB_SEP
+
+NOW_TIME = int(time.time()) * SEC_M
+
+TIME_TABLE = dict()
+TIME_TABLE["s"] = SEC_M
+TIME_TABLE["m"] = TIME_TABLE["s"] * MIN_S
+TIME_TABLE["h"] = TIME_TABLE["m"] * HOUR_M
+TIME_TABLE["d"] = TIME_TABLE["h"] * DAY_H
+TIME_TABLE["w"] = TIME_TABLE["d"] * WEEK_D
+
+TIME_PARAMS = dict()
+
+if ARGS.MY_SECRET:
+    (MY_APINAME, MY_APISECRET) = ARGS.MY_SECRET.split(':')
+    os.environ['SUMO_UID'] = MY_APINAME
+    os.environ['SUMO_KEY'] = MY_APISECRET
+
+if ARGS.MY_CLIENT:
+    (MY_DEPLOYMENT, MY_ORGID) = ARGS.MY_CLIENT.split('_')
+    os.environ['SUMO_LOC'] = MY_DEPLOYMENT
+    os.environ['SUMO_ORG'] = MY_ORGID
+    os.environ['SUMO_TAG'] = ARGS.MY_CLIENT
+
+if ARGS.MY_ENDPOINT:
+    os.environ['SUMO_END'] = ARGS.MY_ENDPOINT
+else:
+    os.environ['SUMO_END'] = os.environ['SUMO_LOC']
+
+try:
+    SUMO_UID = os.environ['SUMO_UID']
+    SUMO_KEY = os.environ['SUMO_KEY']
+    SUMO_LOC = os.environ['SUMO_LOC']
+    SUMO_ORG = os.environ['SUMO_ORG']
+    SUMO_END = os.environ['SUMO_END']
+except KeyError as myerror:
+    print('Environment Variable Not Set :: {} '.format(myerror.args[0]))
+
+PPRINT = pprint.PrettyPrinter(indent=4)
+
+def main():
+    """
+    Setup the Sumo API connection, using the required tuple of region, id, and key.
+    Once done, then issue the command required
+    """
+
+    src = SumoApiClient(SUMO_UID, SUMO_KEY, SUMO_END)
+
+    time_params = calculate_range()
+
+def calculate_range():
+    """
+    This calculates time ranges based on range from current day
+    If specified "NNX to MMY" then NNX is start and MMY is finish
+    """
+
+    number = 1
+    period = "h"
+
+    if ARGS.MY_RANGE:
+        number = re.match(r'\d+', ARGS.MY_RANGE.replace('-', ''))
+        period = ARGS.MY_RANGE.replace(number.group(), '')
+
+    time_to = NOW_TIME
+    time_from = time_to - (int(number.group()) * int(TIME_TABLE[period]))
+    TIME_PARAMS["time_to"] = time_to
+    TIME_PARAMS["time_from"] = time_from
+    TIME_PARAMS["time_zone"] = 'UTC'
+    TIME_PARAMS["by_receipt_time"] = False
+    return TIME_PARAMS
+
+class SumoApiClient():
+    """
+    This is defined SumoLogic API Client
+    The class includes the HTTP methods, cmdlets, and init methods
+    """
+
+    def __init__(self, access_id, access_key, region, cookieFile='cookies.txt'):
+        """
+        Initializes the Sumo Logic object
+        """
+        self.session = requests.Session()
+        self.session.auth = (access_id, access_key)
+        self.session.headers = {'content-type': 'application/json', \
+            'accept': 'application/json'}
+        self.endpoint = 'https://api.' + region + '.sumologic.com/api'
+        cookiejar = http.cookiejar.FileCookieJar(cookieFile)
+        self.session.cookies = cookiejar
+
+    def delete(self, method, params=None, headers=None, data=None):
+        """
+        Defines a Sumo Logic Delete operation
+        """
+        response = self.session.delete(self.endpoint + method, \
+            params=params, headers=headers, data=data)
+        if response.status_code != 200:
+            response.reason = response.text
+        response.raise_for_status()
+        return response
+
+    def get(self, method, params=None, headers=None):
+        """
+        Defines a Sumo Logic Get operation
+        """
+        response = self.session.get(self.endpoint + method, \
+            params=params, headers=headers)
+        if response.status_code != 200:
+            response.reason = response.text
+        response.raise_for_status()
+        return response
+
+    def post(self, method, data, headers=None, params=None):
+        """
+        Defines a Sumo Logic Post operation
+        """
+        response = self.session.post(self.endpoint + method, \
+            data=json.dumps(data), headers=headers, params=params)
+        if response.status_code != 200:
+            response.reason = response.text
+        response.raise_for_status()
+        return response
+
+    def put(self, method, data, headers=None, params=None):
+        """
+        Defines a Sumo Logic Put operation
+        """
+        response = self.session.put(self.endpoint + method, \
+            data=json.dumps(data), headers=headers, params=params)
+        if response.status_code != 200:
+            response.reason = response.text
+        response.raise_for_status()
+        return response
+
+### included code
+
+### included code
+
+
+if __name__ == '__main__':
+    main()
